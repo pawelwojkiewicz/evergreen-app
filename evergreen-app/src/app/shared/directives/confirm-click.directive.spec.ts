a16e781678fba859226884bef27b40dd15485bcf
pawelwojkiewicz <p.wojkiewicz@gmail.com>
2021-08-09 14:22:31 +0200

    deleted testes

--- evergreen-app/src/app/shared/directives/confirm-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConfirmClickDirective } from './confirm-click.directive';
-
-describe('ConfirmClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ConfirmClickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- evergreen-app/src/app/shared/pipes/to-slug.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToSlugPipe } from './to-slug.pipe';
-
-describe('ToSlugPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToSlugPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
